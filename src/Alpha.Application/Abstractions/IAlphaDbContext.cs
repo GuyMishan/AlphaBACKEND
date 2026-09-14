e4616455727b5a9090bda0d8de4f38e6e9f9ca8c
Guy Mishan <73220144+GuyMishan@users.noreply.github.com>
2026-09-14 15:48:35 +0300

    Expose report payments in db context

--- src/Alpha.Application/Abstractions/IAlphaDbContext.cs
+++ src/Alpha.Application/Abstractions/IAlphaDbContext.cs
@@ -21,6 +21,7 @@ public interface IAlphaDbContext
     DbSet<ManualReportEmployee> ManualReportEmployees { get; }
     DbSet<ManualReportProduct> ManualReportProducts { get; }
     DbSet<ManualContribution> ManualContributions { get; }
+    DbSet<ManualReportPayment> ManualReportPayments { get; }
     DbSet<AuditEvent> AuditEvents { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
