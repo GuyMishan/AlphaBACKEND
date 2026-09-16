1346769a65bba0df988c694030ecc5fabceeb2bf
Guy Mishan <73220144+GuyMishan@users.noreply.github.com>
2026-09-16 11:21:54 +0300

    Expose Employer Interface product metadata persistence

--- src/Alpha.Application/Abstractions/IAlphaDbContext.cs
+++ src/Alpha.Application/Abstractions/IAlphaDbContext.cs
@@ -24,6 +24,7 @@ public interface IAlphaDbContext
     DbSet<ManualReportProduct> ManualReportProducts { get; }
     DbSet<ManualContribution> ManualContributions { get; }
     DbSet<ManualReportPayment> ManualReportPayments { get; }
+    DbSet<EmployerInterfaceReportProductData> EmployerInterfaceReportProductData { get; }
     DbSet<ReportTransmission> ReportTransmissions { get; }
     DbSet<EmployerInterfaceFeedback> EmployerInterfaceFeedback { get; }
     DbSet<ContributionPercentageLimit> ContributionPercentageLimits { get; }
