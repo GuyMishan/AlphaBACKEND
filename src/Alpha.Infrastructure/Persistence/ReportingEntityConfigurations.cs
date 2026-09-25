85e0eec174237bd637ba25b73db4683b125294b7
Guy Mishan <73220144+GuyMishan@users.noreply.github.com>
2026-09-25 08:22:27 +0300

    feat(reporting): map 006 employee snapshot fields

--- src/Alpha.Infrastructure/Persistence/ReportingEntityConfigurations.cs
+++ src/Alpha.Infrastructure/Persistence/ReportingEntityConfigurations.cs
@@ -77,6 +77,15 @@ public sealed class ManualReportEmployeeConfiguration : IEntityTypeConfiguration
         b.Property(x => x.LastName).HasMaxLength(100).IsRequired();
         b.Property(x => x.EmployeeNumber).HasMaxLength(50).IsRequired();
         b.Property(x => x.MonthlySalary).HasPrecision(18, 2);
+        b.Property(x => x.InterfaceIdentifier).HasMaxLength(60).IsRequired();
+        b.Property(x => x.EmailSnapshot).HasMaxLength(100);
+        b.Property(x => x.MobileSnapshot).HasMaxLength(30);
+        b.Property(x => x.CitySnapshot).HasMaxLength(120);
+        b.Property(x => x.StreetSnapshot).HasMaxLength(120);
+        b.Property(x => x.HouseNumberSnapshot).HasMaxLength(30);
+        b.Property(x => x.ApartmentSnapshot).HasMaxLength(30);
+        b.Property(x => x.PostalCodeSnapshot).HasMaxLength(20);
+        b.Property(x => x.PostOfficeBoxSnapshot).HasMaxLength(30);
         b.Property(x => x.ValidationStatus).HasConversion<string>().HasMaxLength(40);
         b.Property(x => x.ValidationError).HasMaxLength(2000);
         b.HasIndex(x => new { x.ReportId, x.EmploymentId }).IsUnique();
